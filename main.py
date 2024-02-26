9d3cd3e30f180d47405f9104308631689752486c
Lisa Chen <lisachen011299@gmail.com>
2024-02-26 01:17:22 +0000

    test pipeline

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, redirect, jsonify, session, render_template
 import os
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv()  # Load variables from .env file
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 app = Flask(__name__)
-
+CORS(app) 
 
 @app.route('/')
 def index():
