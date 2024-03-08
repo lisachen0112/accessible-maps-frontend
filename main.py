0bf6a6587430f513dfea01e26e067fa3596c6701
Lisa Chen <lisachen011299@gmail.com>
2024-03-08 15:33:56 +0000

    docker compose

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from flask_cors import CORS
 
 load_dotenv()  # Load variables from .env file
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+PLACES_API_URL = os.getenv('PLACES_API_URL')
 app = Flask(__name__)
 CORS(app) 
 
 @app.route('/')
 def index():
-    return render_template('index.html', GOOGLE_API_KEY=GOOGLE_API_KEY)
+    return render_template('index.html', GOOGLE_API_KEY=GOOGLE_API_KEY, PLACES_API_URL=PLACES_API_URL)
